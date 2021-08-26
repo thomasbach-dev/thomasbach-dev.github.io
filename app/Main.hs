81bb3d89fdf8244b64164a36b730b48fbfe5135b
Thomas Bach <info@thomasbach.dev>
2021-08-26 23:20:58 +0200

    feat: first sites imported from previous website

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,70 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified GHC.IO.Encoding as E
 
+import           Data.Monoid     (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  E.setLocaleEncoding E.utf8
+  hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+    match "static/*" $ do
+        route idRoute
+        compile copyFileCompiler
+    match (fromList ["about.org", "contact.org"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+    match (fromGlob "posts/20*.org") $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Home"                `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
