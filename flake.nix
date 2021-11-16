ecf55b5b41a723861c67df3d1f9b49b43706a740
Thomas Bach <info@thomasbach.dev>
2021-11-16 11:29:13 +0100

    build: downgrade to ghc-8.8.4 to be able to pull in gogol libraries

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
           thomasbach-dev =
             final.haskell-nix.project' {
               src = ./.;
-              compiler-nix-name = "ghc8107";
+              compiler-nix-name = "ghc884";
               # This is used by `nix develop .` to open a shell for use with
               # `cabal`, `hlint` and `haskell-language-server`
               shell.tools = {
