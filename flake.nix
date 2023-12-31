579912957ac2fed8541c4b1ff3a1b849eee8e3a7
Thomas Bach <info@thomasbach.dev>
2024-01-01 00:55:05 +0100

    chore: updated to system nix

--- flake.nix
+++ flake.nix
@@ -1,29 +1,23 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/0b3d618173114c64ab666f557504d6982665d328";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        ghcAndPkgs = pkgs.haskellPackages.ghcWithPackages (ps: [
-          ps.clay
-          ps.hakyll
-          ps.witch
-        ]);
+        ghcAndPkgs = pkgs.haskellPackages.ghcWithPackages
+          (ps: [ ps.clay ps.hakyll ps.witch ]);
       in {
         packages = rec {
-          thomasbach-dev = pkgs.haskellPackages.callPackage (import ./thomasbach-dev.nix) {};
+          thomasbach-dev =
+            pkgs.haskellPackages.callPackage (import ./thomasbach-dev.nix) { };
           default = thomasbach-dev;
         };
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            ghcAndPkgs
-            cabal-install
-            ormolu
-          ];
+          buildInputs = with pkgs; [ ghcAndPkgs cabal-install ormolu ];
         };
-      }
-    );
+      });
 }
